7eb0e801b48478ed33ed8c25a786026c59c4d248
Corey Knight <corey.knight@learnosity.com>
2016-01-28 09:45:07 +1100

    i don't even

--- binary_valid_tree.hs
+++ binary_valid_tree.hs
@@ -9,30 +9,19 @@ tree1 = TreeNode 3 (TreeNode 1 EmptyTree (TreeNode 2 EmptyTree EmptyTree)) (Tree
 
 traverseTree :: (TreeNode a) -> [a] -> [a]
 traverseTree EmptyTree list = list
+traverseTree InvalidTree _ = []
 traverseTree (TreeNode value lhs rhs) list = traverseTree lhs (value:(traverseTree rhs list))
 
-makeTree :: Ord a => [a] -> (TreeNode a)
 makeTree [] = EmptyTree
-makeTree (value:values)
-  | head == EmptyTree = (TreeNode value EmptyTree EmptyTree)
-  | treeValue head == Nothing = InvalidTree
-  | value < fromJust (treeValue head) = (TreeNode value EmptyTree head)
-  | value > fromJust (treeValue head) = (TreeNode value head EmptyTree)
-  | otherwise = InvalidTree
-  where head = makeTree values
+makeTree (value:values) = treeAdd (makeTree values) value
 
---makeTree (value:values) =
-  --if (head == EmptyTree)
-      --then TreeNode value EmptyTree EmptyTree
-      --else if (isNothing headValue)
-          --then InvalidTree
-          --else if value > fromJust headValue
-              --then TreeNode value Nothing head
-              --else if value < fromJust headValue
-                  --then TreeNode value head Nothing
-                  --else InvalidTree
-  --where head = makeTree values
-        --headValue = treeValue head
+treeAdd tree value
+  | tree == EmptyTree = TreeNode value EmptyTree EmptyTree
+  | tree == InvalidTree = InvalidTree
+  | value < treeValue = TreeNode treeValue (treeAdd left value) right
+  | value > treeValue = TreeNode treeValue left (treeAdd right value)
+  | otherwise = InvalidTree
+  where (TreeNode treeValue left right) = tree
 
 treeValue :: Ord a => TreeNode a -> Maybe a
 treeValue node
@@ -44,20 +33,20 @@ treeValue node
 is_nonzero num = num > 0
 
 printTree input = do
-  let tree = makeTree input
+  let tree = makeTree input :: (TreeNode Int)
   putStrLn $ (show tree) ++ ": " ++ (show $ traverseTree tree [])
+
+dumpTree (TreeNode value left right) indent = 
+  
+
 
 main = do
   let val = traverseTree tree1 []
   print val
   let tree2 = makeTree [5, 3, 9, 1, 8, 4, 22, 11]
-  let val2 = traverseTree tree2 []
+  let val2 = (traverseTree tree2 [] :: [Int])
   print val2
 
-  print $ map is_nonzero [1, 2, 3]
-
-  mapM_ print [1,2,3]
-  
   let inputs = [ 
                   [1, 2, 3],
                   [1, 3, 2],
@@ -73,14 +62,9 @@ main = do
                   [2],
                   [2, 1],
                   [1],
-                  [1, 3]
+                  [1, 3],
+                  [-8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8],
+                  [3, 5, 3]
                ]
   
   mapM_ printTree inputs
-  
-  print $ traverseTree (makeTree [1, 2, 3]) []
-  print $ traverseTree (makeTree [1, 3, 2]) []
-  print $ traverseTree (makeTree [2, 1, 3]) []
-  print $ traverseTree (makeTree [2, 3, 1]) []
-  print $ traverseTree (makeTree [3, 1, 2]) []
-  print $ traverseTree (makeTree [3, 2, 1]) []

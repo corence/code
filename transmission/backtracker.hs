b3640843890feb2248429a8f7394689dc9c94315
Corey Knight <corey.knight@learnosity.com>
2016-01-27 22:32:48 +1100

    splitting into solutions

--- transmission/backtracker.hs
+++ transmission/backtracker.hs
@@ -10,9 +10,40 @@ pair456 x = choose [(x,4), (x,5), (x,6)]
 join :: Choice (Choice a) -> Choice a
 join choices = concat choices
 
+bind :: Choice Int -> (Int -> Choice (Int, Int)) -> Choice (Int, Int)
+bind items function = join (map function items)
+
+unit :: a -> Choice a
+unit a = choose [a]
+
+solution1 = map pair456 (choose [1,2,3])
+solution2 = join (map pair456 (choose [1,2,3]))
+solution3 = bind (choose [1,2,3]) pair456
+solution4 = map (choose [1,2,3]) (\x -> map (choose [4,5,6] (\y -> (x,y))))
+solution5 = bind
+              (choose [1,2,3])
+              (\x ->
+                bind
+                  (choose [4,5,6])
+                  (\y ->
+                    unit (x,y)
+                  )
+              )
+
+{-
+ - function solution4() {
+ -     bind(choose([1,2,3]), function(x) {
+ -         bind(choose([4,5,6]), function(y) {}}}
+ -             
+ -}
+ 
+
 main :: IO ()
 main = do
-    let q = join (map pair456 (choose [1,2,3]))
+    --let q = solution1
+    --let q = solution2
+    --let q = solution3
+    let q = solution4
     putStrLn $ (show q)
 
 {-

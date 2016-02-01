0ce123ba99898ebc287372e8c7e87feb2999ceb9
Corey Knight <corey.knight@learnosity.com>
2016-02-02 09:06:54 +1100

    models of computation

--- transmission/backtrack2.hs
+++ transmission/backtrack2.hs
@@ -1,4 +1,17 @@
 
+-- the goal: to write code in this style
+-- solveConstraint = do
+--   x <- choose [1,2,3]
+--   y <- choose [4,5,6]
+--   guard (x*y == 8)
+--   return (x,y)
+
+-- step 1: implement the functionality
+--      1a: generate a list will all combinations of [1,2,3] and [4,5,6]
+--      1b: filter elements out of that list
+-- step 2: implement the syntax (as close as we can get it without monads)
+-- step 3: implement the syntax with monads
+
 type Choice a = [a]
 
 choose :: [a] -> Choice a
@@ -6,25 +19,31 @@ choose xs = xs
 
 solution1 :: Choice (Int, Int)
 solution1 = [(1,4), (1,5), (1,6), (2,4), (2,5), (2,6), (3,4), (3,5), (3,6)]
--- solution1: [(1,4),(1,5),(1,6),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6)]
 
 pairValues x array = map (\y -> (x, y)) array
 
 solution2 :: Choice (Int, Int)
 solution2 = pairValues 1 [4,5,6]
--- solution2: [(1,4),(1,5),(1,6)]
 
 solution3 :: [Choice (Int, Int)]
 solution3 = map (\x -> pairValues x [4,5,6]) [1,2,3]
--- solution3: [[(1,4),(1,5),(1,6)],[(2,4),(2,5),(2,6)],[(3,4),(3,5),(3,6)]]
 
---join :: Choice (Choice a) -> Choice a
 join :: [[a]] -> [a]
 join choices = concat choices
 
 solution4 :: Choice (Int, Int)
 solution4 = join (map (\x -> pairValues x [4,5,6]) [1,2,3])
--- solution4: [(1,4),(1,5),(1,6),(2,4),(2,5),(2,6),(3,4),(3,5),(3,6)]
+
+interleave :: [[Int]] -> [Int] -> [[Int]]
+interleave homers intruders = concat (map (newHomers homers) intruders)
+  where newHomer intruder homer = intruder : homer
+        newHomers homers intruder = map (newHomer intruder) homers
+
+solution5 :: Choice [Int]
+solution5 = interleave [[]] [4,5,6]
+
+solution6 :: Choice [Int]
+solution6 = interleave solution5 [1,2,3]
 
 backtracker :: Choice (Int, Int)
 backtracker = filter (\(x, y) -> x + y == 8) solution4
@@ -38,3 +57,5 @@ main = do
   putStrLn $ "solution3: " ++ (show solution3)
   putStrLn $ "solution4: " ++ (show solution4)
   putStrLn $ "backtracker: " ++ (show backtracker)
+  putStrLn $ "solution5: " ++ (show solution5)
+  putStrLn $ "solution6: " ++ (show solution6)

ec0448b67d1876c2f2491e7cce3ec94846dd8cfb
Corey Knight <corey.knight@learnosity.com>
2017-01-17 14:10:16 +1100

    refactored; now works when prerequisite = target

--- adcap/adcap5.hs
+++ adcap/adcap5.hs
@@ -33,30 +33,33 @@ property_price :: Property -> Rational
 property_price (_, 0, _, _, _) = 0
 property_price (name, quantity, unit_price, coefficient, unit_dps) = unit_price + property_price (name, (quantity - 1), (coefficient * unit_price), coefficient, unit_dps)
 
-find_milestone_breakeven_time_2 :: Milestone -> Property -> Property -> Rational
-find_milestone_breakeven_time_2 milestone prerequisite target = price / dps
-    where price = find_milestone_price milestone prerequisite
-          dps = find_milestone_dps milestone prerequisite target
+find_milestone_breakeven_time :: Milestone -> [Property] -> Rational
+find_milestone_breakeven_time milestone properties = price / dps
+    where price = find_milestone_price milestone properties
+          dps = find_milestone_dps milestone properties
 
-find_milestone_price :: Milestone -> Property -> Rational
-find_milestone_price milestone prerequisite = property_price (p_name, quantity, p_unit_price, p_coefficient, p_unit_dps)
-    where quantity = find_milestone_quantity_needed milestone prerequisite
-          (p_name, _, p_unit_price, p_coefficient, p_unit_dps) = prerequisite
+find_milestone_price :: Milestone -> [Property] -> Rational
+find_milestone_price _ [] = 0
+find_milestone_price milestone (property:properties) = property_price (p_name, diff_quantity, p_unit_price, p_coefficient, p_unit_dps) + find_milestone_price milestone properties
+    where diff_quantity = find_milestone_quantity_needed milestone property
+          (p_name, _, p_unit_price, p_coefficient, p_unit_dps) = property
 
-find_milestone_dps :: Milestone -> Property -> Property -> Rational
-find_milestone_dps milestone prerequisite target = prereq_dps_increase + new_target_dps - old_target_dps
-    where 
-          new_target_dps = new_target_quantity * target_unit_dps * milestone_multiplier
-          old_target_dps = target_quantity * target_unit_dps
-          prereq_dps_increase = prereq_unit_dps * prereq_quantity_diff
-          prereq_quantity_diff = find_milestone_quantity_needed milestone prerequisite
-          (_, _, milestone_quantity, _, milestone_multiplier) = milestone
-          (target_name, target_quantity, _, _, target_unit_dps) = target
-          (prereq_name, _, _, _, prereq_unit_dps) = prerequisite
-          new_target_quantity = if prereq_name == target_name then milestone_quantity else target_quantity
+find_milestone_dps :: Milestone -> [Property] -> Rational          
+find_milestone_dps _ [] = 0
+find_milestone_dps milestone (property:properties) = (new_quantity * multiplied_dps) - (quantity * unit_dps) + find_milestone_dps milestone properties
+    where (_, prerequisite_property_name, prerequisite_property_quantity, target_property_name, multiplier) = milestone
+          (property_name, quantity, unit_price, coefficient, unit_dps) = property
+          new_quantity = if (property_name == prerequisite_property_name && quantity < prerequisite_property_quantity)
+                             then prerequisite_property_quantity
+                             else quantity
+          multiplied_dps = if property_name == target_property_name
+                               then unit_dps * multiplier
+                               else unit_dps
 
 find_milestone_quantity_needed :: Milestone -> Property -> Rational
-find_milestone_quantity_needed (_, _, prerequisite_quantity, _, _) (_, property_quantity, _, _, _) = max 0 (prerequisite_quantity - property_quantity)
+find_milestone_quantity_needed (_, prerequisite_name, prerequisite_quantity, _, _) (property_name, property_quantity, _, _, _) = if prerequisite_name == property_name
+                                                                                                                                     then max 0 (prerequisite_quantity - property_quantity)
+                                                                                                                                     else 0
 
 find_property_dps :: Property -> Rational
 find_property_dps (_, quantity, _, _, unit_dps) = quantity * unit_dps
@@ -67,12 +70,28 @@ main = do
     let milestone = ("Lemon Scented", "Newspaper", 150, "Lemonade Stand", 100 % 50)
     let lemonade = ("Lemonade Stand", 200, 1, 10 % 11, 2)
     let newspaper = ("Newspaper", 147, 10, 100 % 10, 8)
-    putStrLn $ "find_milestone_price: " ++ (show $ fromRational $ find_milestone_price milestone newspaper)
+    putStrLn $ "find_milestone_price: " ++ (show $ fromRational $ find_milestone_price milestone [lemonade, newspaper])
     let old_target_dps = 2 * 200
     let new_target_dps = old_target_dps * 2
     let old_prereq_dps = 8 * 147
     let new_prereq_dps = 8 * 150
+    let dps_increase = find_milestone_dps milestone [lemonade, newspaper]
     putStrLn $ "target_dps -- old: " ++ show old_target_dps ++ ", new: " ++ show new_target_dps
     putStrLn $ "prereq_dps -- old: " ++ show old_prereq_dps ++ ", new: " ++ show new_prereq_dps
-    putStrLn $ "milestone_dps: " ++ (show $ fromRational $ find_milestone_dps milestone newspaper lemonade)
-    putStrLn $ "find_milestone_breakeven_time_2: " ++ (show $ fromRational $ find_milestone_breakeven_time_2 milestone newspaper lemonade)
+    putStrLn $ "milestone_dps: " ++ (show $ fromRational $ find_milestone_dps milestone [newspaper, lemonade])
+    putStrLn $ "dps increase: " ++ (show $ fromRational $ dps_increase)
+    putStrLn $ "find_milestone_breakeven_time: " ++ (show $ fromRational $ find_milestone_breakeven_time milestone [newspaper, lemonade])
+
+    --let milestone2 = ("Theoretical Economist", "All", 500, "All", 2)
+    --putStrLn $ "find_milestone_price: " ++ (show $ fromRational $ find_milestone_price
+
+    let milestone2 = ("Combustible Lemons", "Lemonade Stand", 300, "Lemonade Stand", 2)
+    putStrLn $ "find_milestone_price: " ++ (show $ fromRational $ find_milestone_price milestone2 [lemonade])
+    let old_target_dps_2 = 2 * 200
+    let new_target_dps_2 = old_target_dps * 2
+    let old_prereq_dps_2 = 8 * 147
+    let new_prereq_dps_2 = 8 * 150
+    putStrLn $ "target_dps -- old: " ++ show old_target_dps_2 ++ ", new: " ++ show new_target_dps_2
+    putStrLn $ "prereq_dps -- old: " ++ show old_prereq_dps_2 ++ ", new: " ++ show new_prereq_dps_2
+    putStrLn $ "milestone_dps: " ++ (show $ fromRational $ find_milestone_dps milestone2 [lemonade])
+    putStrLn $ "find_milestone_breakeven_time: " ++ (show $ fromRational $ find_milestone_breakeven_time milestone2 [lemonade])

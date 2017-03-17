aea5f2a915fe0880786b448952426002d3febb7e
Corey Knight <corey.knight@learnosity.com>
2017-03-18 08:17:18 +1100

    part of the structure for running and selecting goals and missions is built :)

--- autonomy/Goals.hs
+++ autonomy/Goals.hs
@@ -22,15 +22,29 @@ module Goals
 
 import qualified Data.Map as Map
 import Data.Map(Map(..))
+import Data.Maybe
 
 type ActorID = Int
 type ThingID = String
 type Pos = Int
 
 type State = (Map ActorID Actor)
-data Actor = Actor ActorID Pos [(Goal, Mission)] (Map ThingID Int) -- ID, position, intentions, inventory
+data Actor = Actor ActorID Pos [(Goal, Mission)] (Map ThingID Int) -- ID, position, intents, inventory
 data Goal = Goal (State -> Bool) (Maybe (State -> Bool)) (State -> [Mission]) -- success, failure, missions_generator
 data Mission = Mission (Maybe (State -> Bool)) [Goal] (State -> State) -- failure, prerequisites, action
+type Intent = (Goal, Mission)
+
+goal_is_succeeding :: Goal -> State -> Bool
+goal_is_succeeding (Goal success _ _) state = success state
+
+goal_is_failing :: Goal -> State -> Bool
+goal_is_failing (Goal _ failure _) state = case failure of { Just ff -> ff state; Nothing -> False }
+
+mission_is_failing :: Mission -> State -> Bool
+mission_is_failing (Mission failure _ _) state = case failure of { Just ff -> ff state; Nothing -> False }
+
+mission_prereqs :: Mission -> [Goal]
+mission_prereqs (Mission _ prereqs _) = prereqs
 
 actor_inv :: ThingID -> Actor -> Int
 actor_inv tid (Actor _ _ _ inventory) = case Map.lookup tid inventory of
@@ -38,8 +52,8 @@ actor_inv tid (Actor _ _ _ inventory) = case Map.lookup tid inventory of
                                            Nothing -> 0
 
 actor_add :: ThingID -> Int -> Actor -> Actor
-actor_add tid amount actor = Actor aid pos intentions (Map.insert tid total inventory)
-    where (Actor aid pos intentions inventory) = actor
+actor_add tid amount actor = Actor aid pos intents (Map.insert tid total inventory)
+    where (Actor aid pos intents inventory) = actor
           total = amount + actor_inv tid actor
 
 actor_sub :: ThingID -> Int -> Actor -> Actor
@@ -52,7 +66,10 @@ actor_pos :: Actor -> Pos
 actor_pos (Actor _ pos _ _) = pos
 
 actor_move :: Pos -> Actor -> Actor
-actor_move new_pos (Actor aid _ intentions inventory) = Actor aid new_pos intentions inventory
+actor_move new_pos (Actor aid _ intents inventory) = Actor aid new_pos intents inventory
+
+actor_add_intent :: Intent -> Actor -> Actor
+actor_add_intent new_intent (Actor aid pos intents inventory) = Actor aid pos (new_intent : intents) inventory
 
 update_actor :: Actor -> State -> State
 update_actor actor actors = Map.insert aid actor actors
@@ -77,3 +94,36 @@ find_actor aid state = case Map.lookup aid state of
 generate :: a -> (b -> a)
 generate a = (\_ -> a)
 
+-- 1) if the actor has no intent, crash
+-- 2) intent is (Goal, Mission)
+-- 3) this means (Goal (State -> Bool) (Maybe (State -> Bool)) (State -> [Mission]), Mission (Maybe (State -> Bool)) [Goal] (State -> State))
+-- 4) if the goal is satisfied, pop it
+-- 5) if the goal is failed, pop it
+-- 6) if the mission is failed, pop it
+-- 7) if any of the mission's prereqs are failing, pick a mission for one of them and push it
+-- 8) if any of the mission's prereqs are failing, and there's no missions available, then pop it
+-- 9) finally, if none of that happened, run the mission's State -> State function!
+
+-- thus: this function needs to be ready to: pick a new mission, update state. Too complex.
+-- splitting it into two instead
+
+update_intents :: State -> [Intent] -> [Intent]
+update_intents state [] = error "new intent generation not implemented yet"
+update_intents state (intent : intents)
+  | goal_is_succeeding goal state = update_intents state intents -- pop
+  | goal_is_failing goal state = update_intents state intents -- pop
+  | mission_is_failing mission state = update_intents state intents -- pop
+  | null unsatisfied_prereqs = intent : intents -- no change here -- this intent pile is ready to run (most common case)
+  | null prereq_solutions = update_intents state intents -- pop because one of the subgoals is failing and there's no way to fix it
+  | otherwise = update_intents state (head prereq_solutions : intent : intents) -- complete a subgoal before completing the main one
+  where (goal, mission) = intent
+        unsatisfied_prereqs = filter (\goal -> not $ goal_is_succeeding goal state) (mission_prereqs mission)
+        prereq_solutions = catMaybes $ map (\goal -> create_intent state goal) unsatisfied_prereqs
+
+create_intent :: State -> Goal -> Maybe Intent
+create_intent state goal = error "generate one of your sub-missions if you can"
+  
+actor_run_intent :: ActorID -> State -> State
+actor_run_intent aid state = mission_action state
+  where Actor _ _ (intent : intents) _ = find_actor aid state
+        (Goal goal_success goal_failure _, Mission mission_failure mission_prereqs mission_action) = intent

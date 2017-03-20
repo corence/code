ef1ac2ed0cf661643ac93850ff6b437b255c724a
Corey Knight <corey.knight@learnosity.com>
2017-03-20 21:59:05 +1100

    partway through conversion of TheSims.hs to no Goals/Missions. This is a mistake.

--- autonomy/Tasks.hs
+++ autonomy/Tasks.hs
@@ -1,6 +1,25 @@
 
 module Tasks
-(
+( Actor(..)
+, ActorID
+, Pos
+, State
+, Task(..)
+, ThingID
+, actor_add
+, actor_id
+, actor_inv
+, actor_move
+, actor_pos
+, actor_sub
+, adjust_actor
+, find_actor
+, find_actors_with
+, find_actors_with_inv
+, query_actor
+, step_tasks
+, replace_actor
+, replace_actors
 ) where
 
 import qualified Data.Map as Map
@@ -14,16 +33,24 @@ type Pos = Int
 type State = (Map ActorID Actor)
 data Actor = Actor ActorID Pos [Task] (Map ThingID Int) -- ID, position, tasks, inventory
 
-data Task = Task String [State -> Bool] [State -> [Task]] (State -> State) -- name, completions, prerequisite_generators, action
+data Task =
+    TGoal String [State -> [Task]] [State -> Bool] -- name, solution_generators, successes
+    | TTask String [State -> [Task]] (State -> State) Bool -- name, prerequisite_generators, action, repeating
 
 instance Show Actor where
     show (Actor aid pos intents inventory) = "Actor #" ++ show aid ++ " @" ++ show pos ++ " intents " ++ show (length intents) ++ " inv " ++ show inventory
 
 task_is_done :: Task -> State -> Bool
-task_is_done (Task _ completions _ _) state = any (\is_complete -> is_complete state) completions
+task_is_done (TGoal _ _ successes) state = any (\success -> success state) successes
+task_is_done (TTask _ _ _ _) state = False
+
+generate_solutions :: Task -> State -> [Task]
+generate_solutions (TGoal _ solution_generators _) state = concat $ map (\solve -> solve state) solution_generators
+generate_solutions (TTask _ _ _ _) _ = []
 
 generate_prerequisites :: Task -> State -> [Task]
-generate_prerequisites (Task _ _ prerequisite_generators _) state = concat $ map (\generate -> generate state) prerequisite_generators
+generate_prerequisites (TGoal _ _ _) _ = []
+generate_prerequisites (TTask _ prerequisite_generators _ _) state = concat $ map (\generate -> generate state) prerequisite_generators
 
 actor_inv :: ThingID -> Actor -> Int
 actor_inv tid (Actor _ _ _ inventory) = case Map.lookup tid inventory of
@@ -47,13 +74,13 @@ actor_pos (Actor _ pos _ _) = pos
 actor_move :: Pos -> Actor -> Actor
 actor_move new_pos (Actor aid _ intents inventory) = Actor aid new_pos intents inventory
 
-update_actor :: Actor -> State -> State
-update_actor actor actors = Map.insert aid actor actors
+replace_actor :: Actor -> State -> State
+replace_actor actor actors = Map.insert aid actor actors
     where Actor aid _ _ _ = actor
 
-update_actors :: [Actor] -> State -> State
-update_actors [] state = state
-update_actors (actor : actors) state = update_actors actors (update_actor actor state)
+replace_actors :: [Actor] -> State -> State
+replace_actors [] state = state
+replace_actors (actor : actors) state = replace_actors actors (replace_actor actor state)
 
 -- converts an (Actor -> Bool) function to a (State -> Bool) function
 query_actor :: ActorID -> (Actor -> a) -> (State -> a)
@@ -63,7 +90,7 @@ query_actor aid function = state_query
 -- find the actor, take action upon her, and return her to the state. Nice
 adjust_actor :: ActorID -> (Actor -> Actor) -> (State -> State)
 adjust_actor aid function = state_action
-    where state_action state = update_actor (function (find_actor aid state)) state
+    where state_action state = replace_actor (function (find_actor aid state)) state
 
 find_actor :: ActorID -> State -> Actor
 find_actor aid state = case Map.lookup aid state of
@@ -78,25 +105,42 @@ find_actors_with_inv tid amount state = find_actors_with predicate state
     where predicate actor = actor_inv tid actor >= amount
 
 -- this will either adjust the dude's current tasks, or it will run one
--- 0) if no tasks are on the stack, create one
--- 1) if the task is done (pass/fail), pop it (this may trigger #0)
--- 2) if any prerequisite tasks are needed, generate one (this will likely trigger #2)
--- 3) otherwise, run the task
-
 step_tasks :: ActorID -> State -> State
 step_tasks dude_id state
-  | dude_has_no_tasks = create_task
-  | dude_task_is_complete = pop_task
-  | null prerequisites = run_task
-  | otherwise = add_prerequisite
+  | null dude_tasks = trace "step_tasks 1" $ create_task
+  | otherwise = case (head dude_tasks) of
+                    (TGoal _ _ _) -> step_goal dude_id state
+                    (TTask _ _ _ _) -> step_task dude_id state
+  where dude = find_actor dude_id state
+        Actor _ dude_pos dude_tasks dude_inventory = dude
+        create_task = error "idk how to create a task yet"
+
+step_goal :: ActorID -> State -> State
+step_goal dude_id state
+  | task_is_done task state = trace "step_goal 1" $ dismiss_this_goal
+  | null solutions = trace "step_goal 2" $ dismiss_this_goal
+  | otherwise = trace "step_goal 3" $ add_solution
   where dude = find_actor dude_id state
         Actor _ dude_pos dude_tasks dude_inventory = dude
         task = head dude_tasks
-        Task task_name task_completions task_generators task_action = task
-        dude_has_no_tasks = null dude_tasks
-        dude_task_is_complete = task_is_done task state
+        solutions = generate_solutions task state
+        add_solution = adjust_actor dude_id (\_ -> Actor dude_id dude_pos (head solutions : dude_tasks) dude_inventory) state -- TODO: this should be smarter than head
+        dismiss_this_goal = replace_actor (actor_pop_task dude) state
+
+actor_pop_task :: Actor -> Actor
+actor_pop_task (Actor aid a_pos a_tasks a_inv) = Actor aid a_pos (tail a_tasks) a_inv
+
+actor_set_tasks :: [Task] -> Actor -> Actor
+actor_set_tasks new_tasks (Actor aid a_pos _ a_inv) = Actor aid a_pos new_tasks a_inv
+
+step_task :: ActorID -> State -> State
+step_task dude_id state
+  | null prerequisites = trace "step_task 1" $ if task_repeating then task_action state else pop_task $ task_action state
+  | otherwise = trace "step_task 2" $ add_prerequisite
+  where dude = find_actor dude_id state
+        Actor _ dude_pos dude_tasks dude_inventory = dude
+        task = head dude_tasks
+        TTask _ _ task_action task_repeating = task
         prerequisites = generate_prerequisites task state
-        create_task = error "idk how to create a task yet"
-        pop_task = adjust_actor dude_id (\_ -> Actor dude_id dude_pos (tail dude_tasks) dude_inventory) state
-        run_task = task_action state
         add_prerequisite = adjust_actor dude_id (\_ -> Actor dude_id dude_pos (head prerequisites : dude_tasks) dude_inventory) state
+        pop_task state = adjust_actor dude_id (\_ -> Actor dude_id dude_pos (tail dude_tasks) dude_inventory) state
--- autonomy/TheSims.hs
+++ autonomy/TheSims.hs
@@ -4,16 +4,15 @@ module TheSims
 , eat
 ) where
 
-import Goals
+import Tasks
 import qualified Data.Map as Map
 import Data.Map(Map(..))
 
-do_goal :: Goal -> Mission
-do_goal goal = Mission Nothing [goal] (error "bad goal for do_goal. it should have already won before trying to execute")
-
--- take multiple functions that convert input to a list, and return a single function that converts that input to a conjoined list
-multi :: [a -> [b]] -> (a -> [b])
-multi generators = (\input -> concat $ map (\generator -> generator input) generators)
+{-
+ - data Task =
+ -     TGoal String [State -> [Task]] [State -> Bool] -- name, solution_generators, successes
+ -     | TTask String [Task] (State -> State) Bool -- name, prerequisites, action, repeating
+ -}
 
 hungry :: Actor -> Bool
 hungry dude = actor_inv "hunger" dude >= 20
@@ -21,69 +20,57 @@ hungry dude = actor_inv "hunger" dude >= 20
 unhungry :: Actor -> Bool
 unhungry dude = not $ hungry dude
 
-be_unhungry :: ActorID -> Goal
-be_unhungry dude_id = Goal (query_actor dude_id unhungry) Nothing (generate $ [eat dude_id])
+be_unhungry :: ActorID -> Task
+be_unhungry dude_id = TTask "be_unhungry" [query_actor dude_id unhungry] [eat dude_id]
 
-eat :: ActorID -> Mission
-eat dude_id = Mission Nothing [have_food 1 dude_id] consume_food
-    where 
-          consume_food state = act dude_id (actor_sub "hunger" 10) $ act dude_id (actor_sub "food" 1) state
+eat :: ActorID -> Task
+eat dude_id = TTask "eat" [have_food 1 dude_id] consume_food False
+    where consume_food state = adjust_actor dude_id (actor_sub "hunger" 10) $ adjust_actor dude_id (actor_sub "food" 1) state
 
-{-
-have_food :: ActorID -> Goal
-have_food dude_id = Goal dude_has_food (Just no_food_exists) (multi [cook, seek_item "food" dude_id [dude_id]])
-    where dude_has_food = query_actor dude_id (\dude -> actor_inv "food" dude > 0)
-          no_food_exists state = not $ any (\a -> actor_inv "food" a > 0) (Map.elems state)
-          -}
-          
-have_food :: Int -> ActorID -> Goal
+have_food :: Int -> ActorID -> Task
 have_food amount dude_id = have_item "food" amount [generate_cook_missions dude_id] dude_id
           
-have_item :: ThingID -> Int -> [State -> [Mission]] -> ActorID -> Goal
-have_item tid amount mission_generators dude_id = Goal dude_has_thing (Just no_thing_exists) (multi (seek_item tid dude_id [dude_id] : mission_generators))
-    where dude_has_thing state = query_actor dude_id (\dude -> actor_inv tid dude >= amount) state
-          no_thing_exists state = sum (map (\actor -> actor_inv tid actor) (Map.elems state)) < amount
+have_item :: ThingID -> Int -> [State -> [Task]] -> ActorID -> Task
+have_item tid amount task_generators dude_id = TTask "have_item" already_has_item [seek_item tid dude_id [dude_id]] -- todo: generators!
+    where already_has_item = (query_actor dude_id (\dude -> actor_inv tid dude >= amount))
 
 -- 2 ways we can do this:
 -- 1) foreach oven, generate a mission to cook food in it
 -- 2) generate one mission that will constantly scan for ovens and move closer to them
 -- we picked the first one
-generate_cook_missions :: ActorID -> State -> [Mission]
+generate_cook_missions :: ActorID -> State -> [Task]
 generate_cook_missions dude_id state = map make_mission oven_ids
     where oven_ids = map actor_id (find_actors_with_inv "oven" 1 state)
-          make_mission oven_id = Mission (Just $ failure_conditions oven_id) [have_item "prepped_ingredients" 1 [] dude_id, be_at oven_id dude_id] (make_food oven_id)
-          failure_conditions oven_id state = oven_no_longer_exists oven_id state &&  dude_doesnt_have_item state
-          dude_doesnt_have_item state = actor_inv "prepped_ingredients" (find_actor dude_id state) <= 0
-          oven_no_longer_exists oven_id state = False -- TODO
+          make_mission oven_id = TTask "cook in this oven" [have_item "prepped_ingredients" 1 [] dude_id, be_at oven_id dude_id] (make_food oven_id) -- TODO: mission generators
           -- make_food: decrement the dude's prepped ingredients; increment the oven's food
-          make_food oven_id state = update_actors [actor_sub "prepped_ingredients" 1 (find_actor dude_id state), actor_add "food" 1 (find_actor oven_id state)] state
+          make_food oven_id state = replace_actors [actor_sub "prepped_ingredients" 1 (find_actor dude_id state), actor_add "food" 1 (find_actor oven_id state)] state
 
-seek_item :: ThingID -> ActorID -> [ActorID] -> State -> [Mission]
+seek_item :: ThingID -> ActorID -> [ActorID] -> State -> [Task]
 seek_item tid dude_id blacklist state = take_item_missions
     where actors_with_item = filter (\target -> actor_inv tid target > 0) (Map.elems state)
           aids_with_item = map actor_id actors_with_item
           take_item_missions = map (\target -> take_item tid target dude_id) aids_with_item
 
-take_item :: ThingID -> ActorID -> ActorID -> Mission
-take_item tid target_id dude_id = Mission (Just target_no_longer_has_it) [be_at target_id dude_id] exchange
-    where target_no_longer_has_it = query_actor target_id (\target -> actor_inv tid target <= 0)
-          exchange state = act dude_id (actor_add tid 1) $ act target_id (actor_sub tid 1) state
+take_item :: ThingID -> ActorID -> ActorID -> Task
+take_item tid target_id dude_id = TTask name [be_at target_id dude_id] exchange False
+    where name = ("take_item " ++ show tid ++ " from " ++ show target_id)
+          exchange state = adjust_actor dude_id (actor_add tid 1) $ adjust_actor target_id (actor_sub tid 1) state
 
-be_at :: ActorID -> ActorID -> Goal
-be_at target dude_id = Goal same_place Nothing (generate [go_to target dude_id])
-    where same_place state = query_actor dude_id actor_pos state == query_actor target actor_pos state
+be_at :: ActorID -> ActorID -> Task
+be_at target_id dude_id = TGoal ("be at " ++ show target_id) [\_ -> [go_to target_id dude_id]] [same_place]
+    where same_place state = query_actor dude_id actor_pos state == query_actor target_id actor_pos state
 
 {-
-be_at_actor_with :: ThingID -> Int -> ActorID -> Goal
-be_at_actor_with tid amount dude_id = Goal any_matches Nothing missions
+be_at_actor_with :: ThingID -> Int -> ActorID -> Task
+be_at_actor_with tid amount dude_id = Task any_matches Nothing missions
     where any_matches state = let dude = find_actor dude_id state in any (\target_id -> dude_matches dude (find_actor target_id state))
           dude_matches dude target = actor_pos dude == actor_pos target && actor_inv tid target >= amount && actor_id target /= actor_id dude
           missions state = map (\target_id -> go_to target_id dude_id) (target_ids state)
           target_ids state = map actor_id $ filter (\target -> 
 -}
 
-go_to :: ActorID -> ActorID -> Mission
-go_to dude_id target = Mission Nothing [] update_pos
+go_to :: ActorID -> ActorID -> Task
+go_to dude_id target = TTask "goto" [] update_pos True
     where new_pos state = query_actor target actor_pos state
-          update_pos state = act dude_id (actor_move (new_pos state)) state
+          update_pos state = adjust_actor dude_id (actor_move (new_pos state)) state
 

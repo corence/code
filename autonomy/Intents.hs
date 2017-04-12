6b34ae7179d80b1b0eea831b3082883fc4899715
Corey Knight <corey.knight@learnosity.com>
2017-04-12 17:48:59 +1000

    midway through converting Command and State to use Control.Monad.State -- and it's disgusting, I don't like it.

--- autonomy/Intents.hs
+++ autonomy/Intents.hs
@@ -1,6 +1,7 @@
 
 module Intents
-( Goal(..)
+( Command(..)
+, Goal(..)
 , Intent(..)
 , Task(..)
 , intents_extract_actions
@@ -19,6 +20,8 @@ import qualified Data.Map as Map
 import Data.Map(Map(..))
 import Debug.Trace
 
+import Control.Monad.State
+
 --ctrace = trace
 ctrace _ = id
 
@@ -26,75 +29,76 @@ ctrace _ = id
 -- if i have at least one intent, then look at the head:
 ---- does the Goal pass? if so, pop this intent
 ---- is there Nothing for the task_options? in that case, the Goal needs to generate a bunch of them
----- is there [] for the task_options? if so, CLEAR the whole intent stack (we've reached a failure state and we're at risk of endless looping now)
+---- is there [] for the task_options? if so, CLEAR the whole intent stack (we've reached a failure world and we're at risk of endless looping now)
 ---- is there (x:[]) for the task_options? perfect; this stack is ready to execute
 ---- otherwise there are too many task_options; we need to filter some out
-data Intent command state
-    = HazyIntent (Goal command state)
-    | OptionyIntent (Goal command state) [Task command state]
-    | TaskIntent (Goal command state) (Task command state)
-    | ExecutableIntent (Goal command state) (Task command state)
+type Command world = world -> (world, ())
+data Goal world = Goal String [world -> [Task world]] [world -> Bool] -- name, task_generators, win conditions (need all for success)
+data Task world = Task String [Goal world] [Command world] (world -> Float) -- name, prerequisites, actions, cost of the actions
+data Intent world
+    = HazyIntent (Goal world)
+    | OptionyIntent (Goal world) [Task world]
+    | TaskIntent (Goal world) (Task world)
+    | ExecutableIntent (Goal world) (Task world)
     deriving Show
-data Goal command state = Goal String [state -> [Task command state]] [state -> Bool] -- name, task_generators, win conditions (need all for success)
-data Task command state = Task String [Goal command state] [command] (state -> Float) -- name, prerequisites, actions, cost of the actions
 
-instance Show (Task command state) where
+instance Show (Task world) where
     show (Task name _ _ _) = "Task " ++ name
 
-instance Show (Goal command state) where
+instance Show (Goal world) where
     show (Goal name _ _) = "Goal " ++ name
 
 type ActorID = Int
 
-intents_ready :: [Intent command state] -> Bool
+intents_ready :: [Intent world] -> Bool
 intents_ready [] = False
 intents_ready (ExecutableIntent _ _ : _) = True
 intents_ready _ = False
 
-intents_extract_actions :: [Intent command state] -> ([Intent command state], [command])
+intents_extract_actions :: [Intent world] -> ([Intent world], [Command world])
 intents_extract_actions [] = ([], [])
 intents_extract_actions (intent : other_intents) = case intent of
     ExecutableIntent goal task -> (HazyIntent goal : other_intents, task_actions task)
     otherwise -> (intent : other_intents, [])
 
-prepare_intents :: [Intent command state] -> state -> [Intent command state]
+prepare_intents :: [Intent world] -> world -> [Intent world]
 prepare_intents [] _ = ctrace "    time to generate a fresh new intent " $ []
-prepare_intents (intent : intents) state = case intent of
-    HazyIntent goal -> if goal_succeeds goal state
+prepare_intents (intent : intents) world = case intent of
+    HazyIntent goal -> if goal_succeeds goal world
                        then ctrace ("    " ++ goal_name goal ++ ": popping successful intent ") $ intents
-                       else ctrace ("    " ++ goal_name goal ++ ": generating task options: " ++ show (map task_name (goal_generate_tasks goal state))) $ OptionyIntent goal (goal_generate_tasks goal state) : intents
+                       else ctrace ("    " ++ goal_name goal ++ ": generating task options: " ++ show (map task_name (goal_generate_tasks goal world))) $ OptionyIntent goal (goal_generate_tasks goal world) : intents
     OptionyIntent goal [] -> ctrace ("    " ++ goal_name goal ++ ": hitting a wall! ") $ []
     OptionyIntent goal tasks -> ctrace ("    " ++ goal_name goal ++ ": winnowing tasks down to " ++ task_name (select_task tasks)) $ TaskIntent goal (select_task tasks) : intents
     TaskIntent goal task -> if null incomplete_prerequisites
                             then ctrace ("    " ++ goal_name goal ++ ": no prerequisites ") $ ExecutableIntent goal task : intents
                             else ctrace ("    " ++ goal_name goal ++ ": preparing prerequisites ") $ HazyIntent (head incomplete_prerequisites) : intent : intents
-                            where incomplete_prerequisites = task_incomplete_prerequisites task state
+                            where incomplete_prerequisites = task_incomplete_prerequisites task world
     ExecutableIntent goal _ -> ctrace ("    " ++ goal_name goal ++ ": ready to execute ") $ intent : intents
 
-goal_succeeds :: Goal command state -> state -> Bool
-goal_succeeds (Goal _ _ win_conditions) state = all (\condition -> condition state) win_conditions
+goal_succeeds :: Goal world -> world -> Bool
+goal_succeeds (Goal _ _ win_conditions) world = all (\condition -> condition world) win_conditions
 
-goal_generate_tasks :: Goal command state -> state -> [Task command state]
-goal_generate_tasks (Goal _ task_generators _) state = concat $ map (\generate_tasks -> generate_tasks state) task_generators
+goal_generate_tasks :: Goal world -> world -> [Task world]
+goal_generate_tasks (Goal _ task_generators _) world = concat $ map (\generate_tasks -> generate_tasks world) task_generators
 
-goal_name :: Goal command state -> String
+goal_name :: Goal world -> String
 goal_name (Goal name _ _) = name
 
-task_name :: Task command state -> String
+task_name :: Task world -> String
 task_name (Task name _ _ _) = name
 
-task_prerequisites :: Task command state -> [Goal command state]
+task_prerequisites :: Task world -> [Goal world]
 task_prerequisites (Task _ prerequisites _ _) = prerequisites
 
-task_cost :: Task command state -> state -> Float
+task_cost :: Task world -> world -> Float
 task_cost (Task _ _ _ cost) = cost
 
-task_incomplete_prerequisites :: Task command state -> state -> [Goal command state]
-task_incomplete_prerequisites task state = filter (\goal -> not (goal_succeeds goal state)) (task_prerequisites task)
+task_incomplete_prerequisites :: Task world -> world -> [Goal world]
+task_incomplete_prerequisites task world = filter (\goal -> not (goal_succeeds goal world)) (task_prerequisites task)
 
-task_actions :: Task command state -> [command]
+task_actions :: Task world -> [Command world]
 task_actions (Task _ _ actions _) = actions
 
-select_task :: [Task command state] -> Task command state
+select_task :: [Task world] -> Task world
 select_task [] = error "can't select_task, empty list"
 select_task tasks = head tasks
--- autonomy/TheSims.hs
+++ autonomy/TheSims.hs
@@ -9,12 +9,12 @@
 
 module TheSims
 ( Command(..)
-, State(..)
+, World(..)
 , actor_exists
 , be_at
 , be_unhungry
 , cook
-, dump_state
+, dump_world
 , eat
 , have_food
 , have_item
@@ -29,40 +29,40 @@ import qualified Actor as Actor
 import Actor(ActorID, Actor(..), ItemID, Pos)
 import qualified Data.Map as Map
 import Data.Map(Map(..))
+import Control.Monad.State
 
-type State = Map ActorID Actor
-type Command = State -> State
+type World = Map ActorID Actor
 
-dump_state :: State -> String
-dump_state state = dump_actors $ Map.elems state
+dump_world :: World -> String
+dump_world world = dump_actors $ Map.elems world
 
 dump_actors :: [Actor] -> String
 dump_actors actors = (actors >>= ("\n    " ++) . show) -- ++ "\n"
 
-find_actor :: ActorID -> State -> Actor
-find_actor actor_id state = case Map.lookup actor_id state of
+find_actor :: ActorID -> World -> Actor
+find_actor actor_id world = case Map.lookup actor_id world of
                                Just actor -> actor
                                Nothing -> error $ "find_actor: actor not found: " ++ show actor_id
 
-actor_exists :: ActorID -> State -> Bool
+actor_exists :: ActorID -> World -> Bool
 actor_exists = Map.member
 
-find_actors_with :: (Actor -> Bool) -> State -> [Actor]
-find_actors_with predicate state = filter predicate (Map.elems state)
+find_actors_with :: (Actor -> Bool) -> World -> [Actor]
+find_actors_with predicate world = filter predicate (Map.elems world)
 
-query_actor :: ActorID -> (Actor -> a) -> State -> a
-query_actor actor_id query state = query (find_actor actor_id state)
+query_actor :: ActorID -> (Actor -> a) -> World -> a
+query_actor actor_id query world = query (find_actor actor_id world)
 
-adjust_actor :: ActorID -> (Actor -> Actor) -> State -> State
-adjust_actor actor_id adjust state = Map.insert actor_id (adjust (find_actor actor_id state)) state
+adjust_actor :: ActorID -> (Actor -> Actor) -> World -> (World, ())
+adjust_actor actor_id adjust world = Map.insert actor_id (adjust (find_actor actor_id world)) world
 
-adjust_actors :: [(ActorID, (Actor -> Actor))] -> State -> State
-adjust_actors [] state = state
-adjust_actors ((actor_id, adjust) : adjusters) state = adjust_actors adjusters new_state
-    where new_state = adjust_actor actor_id adjust state
+adjust_actors :: [(ActorID, (Actor -> Actor))] -> World -> (World, ())
+adjust_actors [] world = (world, ())
+adjust_actors ((actor_id, adjust) : adjusters) world = adjust_actors adjusters new_world
+    where new_world = adjust_actor actor_id adjust world
 
---replace_actor :: Actor -> State -> State
---replace_actor actor state = Map.insert (actor_id actor) actor state
+--replace_actor :: Actor -> World -> World
+--replace_actor actor world = Map.insert (actor_id actor) actor world
 
 hungry :: Actor -> Bool
 hungry actor = Actor.get_item "hunger" actor >= 20
@@ -70,17 +70,17 @@ hungry actor = Actor.get_item "hunger" actor >= 20
 unhungry :: Actor -> Bool
 unhungry actor = not $ hungry actor
 
-be_unhungry :: ActorID -> Goal Command State
+be_unhungry :: ActorID -> Goal World
 be_unhungry actor_id = Goal "be_unhungry" [\_ -> [eat actor_id]] [query_actor actor_id unhungry]
 
-eat :: ActorID -> Task Command State
+eat :: ActorID -> Task World
 eat actor_id = Task "eat" [have_food 1 actor_id] [consume_food] (const 1)
-    where consume_food state = adjust_actors [(actor_id, Actor.sub_item "hunger" 10), (actor_id, Actor.sub_item "food" 1)] state
+    where consume_food world = adjust_actors [(actor_id, Actor.sub_item "hunger" 10), (actor_id, Actor.sub_item "food" 1)] world
 
-have_food :: Int -> ActorID -> Goal Command State
+have_food :: Int -> ActorID -> Goal World
 have_food amount actor_id = have_item "food" amount [generate_cook_tasks actor_id] actor_id
 
-have_item :: ItemID -> Int -> [State -> [Task Command State]] -> ActorID -> Goal Command State
+have_item :: ItemID -> Int -> [World -> [Task World]] -> ActorID -> Goal World
 have_item item_id amount task_generators actor_id = Goal "have_item" (seek_item item_id actor_id [actor_id] : task_generators) [already_has_item] -- TODO: generators!
     where already_has_item = (query_actor actor_id (\actor -> Actor.get_item item_id actor >= amount))
 
@@ -88,46 +88,46 @@ have_item item_id amount task_generators actor_id = Goal "have_item" (seek_item 
 -- 1) foreach oven, generate a mission to cook food in it
 -- 2) generate one mission that will constantly scan for ovens and move closer to them
 -- we picked the first one
-generate_cook_tasks :: ActorID -> State -> [Task Command State]
-generate_cook_tasks actor_id state = map (\oven_id -> cook oven_id actor_id) oven_ids
-    where oven_ids = map Actor.get_id (find_actors_with (\actor -> Actor.get_item "oven" actor >= 1) state)
+generate_cook_tasks :: ActorID -> World -> [Task World]
+generate_cook_tasks actor_id world = map (\oven_id -> cook oven_id actor_id) oven_ids
+    where oven_ids = map Actor.get_id (find_actors_with (\actor -> Actor.get_item "oven" actor >= 1) world)
 
-cook :: ActorID -> ActorID -> Task Command State
+cook :: ActorID -> ActorID -> Task World
 cook oven_id actor_id
     = Task ("cook in oven " ++ show oven_id) [have_item "prepped_ingredients" 1 [] actor_id, be_at oven_id actor_id] [make_food oven_id] (const 1) -- TODO: mission generators. Also this should be looked up from somewhere -- surely "cook" isn't the place to encode all the ways to find prepped_ingredients
       -- make_food: decrement the actor's prepped ingredients; increment the oven's food
-    where make_food oven_id state = adjust_actors [
+    where make_food oven_id world = adjust_actors [
                                           (actor_id, Actor.sub_item "prepped_ingredients" 1),
                                           (oven_id, Actor.add_item "food" 1)
-                                          ] state
+                                          ] world
 
-seek_item :: ItemID -> ActorID -> [ActorID] -> State -> [Task Command State]
-seek_item item_id actor_id blacklist state = take_item_tasks
-    where actors_with_item = filter (\target -> Actor.get_item item_id target > 0) (Map.elems state)
+seek_item :: ItemID -> ActorID -> [ActorID] -> World -> [Task World]
+seek_item item_id actor_id blacklist world = take_item_tasks
+    where actors_with_item = filter (\target -> Actor.get_item item_id target > 0) (Map.elems world)
           aids_with_item = map Actor.get_id actors_with_item
           take_item_tasks = map (\target -> take_item item_id target actor_id) aids_with_item
 
-take_item :: ItemID -> ActorID -> ActorID -> Task Command State
+take_item :: ItemID -> ActorID -> ActorID -> Task World
 take_item item_id target_id actor_id = Task name [be_at target_id actor_id] [exchange] (const 0)
     where name = ("take_item '" ++ item_id ++ "' from " ++ show target_id)
-          exchange state = adjust_actors [(actor_id, (Actor.add_item item_id 1)), (target_id, (Actor.sub_item item_id 1))] state
+          exchange world = adjust_actors [(actor_id, (Actor.add_item item_id 1)), (target_id, (Actor.sub_item item_id 1))] world
 
-be_at :: ActorID -> ActorID -> Goal Command State
+be_at :: ActorID -> ActorID -> Goal World
 be_at target_id actor_id = Goal ("be at " ++ show target_id) [\_ -> [go_toward target_id actor_id]] [same_place]
-    where same_place state = query_actor actor_id Actor.get_pos state == query_actor target_id Actor.get_pos state
+    where same_place world = query_actor actor_id Actor.get_pos world == query_actor target_id Actor.get_pos world
 
 {-
 be_at_actor_with :: ItemID -> Int -> ActorID -> Task
 be_at_actor_with item_id amount actor_id = Task any_matches Nothing missions
-    where any_matches state = let actor = find_actor actor_id state in any (\target_id -> actor_matches actor (find_actor target_id state))
+    where any_matches world = let actor = find_actor actor_id world in any (\target_id -> actor_matches actor (find_actor target_id world))
           actor_matches actor target = Actor.get_pos actor == Actor.get_pos target && actor_inv item_id target >= amount && actor_id target /= actor_id actor
-          missions state = map (\target_id -> go_toward target_id actor_id) (target_ids state)
-          target_ids state = map actor_id $ filter (\target -> 
+          missions world = map (\target_id -> go_toward target_id actor_id) (target_ids world)
+          target_ids world = map actor_id $ filter (\target -> 
 -}
 
-go_toward :: ActorID -> ActorID -> Task Command State
-go_toward actor_id target_id = Task "go toward" [] [update_pos] (\state -> fromIntegral $ Actor.distance (old_pos state) (new_pos state))
-    where new_pos state = query_actor target_id Actor.get_pos state
-          old_pos state = query_actor actor_id Actor.get_pos state
-          update_pos state = adjust_actor actor_id (Actor.set_pos (new_pos state)) state
+go_toward :: ActorID -> ActorID -> Task World
+go_toward actor_id target_id = Task "go toward" [] [update_pos] (\world -> fromIntegral $ Actor.distance (old_pos world) (new_pos world))
+    where new_pos world = query_actor target_id Actor.get_pos world
+          old_pos world = query_actor actor_id Actor.get_pos world
+          update_pos world = adjust_actor actor_id (Actor.set_pos (new_pos world)) world
 

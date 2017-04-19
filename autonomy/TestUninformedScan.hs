e2f6c1ce6f107e345af6a2b7d5f5d7bee01cc466
Corey Knight <corey.knight@learnosity.com>
2017-04-19 18:17:03 +1000

    fixing parts of the test. but the queue is flaky! :(

--- autonomy/TestUninformedScan.hs
+++ autonomy/TestUninformedScan.hs
@@ -52,30 +52,44 @@ doit n aheap = if AutoHeap.is_empty aheap
                   else putStrLn (dump_decisions aheap) >> doit (n-1) (advance_resolutions aheap)
 
 dump_decisions :: AutoHeap (PartialResolution World) -> String
-dump_decisions aheap = "\n" ++ show (AutoHeap.size aheap) ++ "\n" ++ dump2 aheap ++ "\n"
+dump_decisions aheap = "\n" ++ show (AutoHeap.size aheap) ++ "\n" ++ dump_each_decision aheap ++ "\n"
 
-dump2 :: AutoHeap (PartialResolution World) -> String
-dump2 aheap = case first of
-                Just pr -> dump3 pr ++ "\n" ++ dump2 others
-                Nothing -> ""
+dump_each_decision :: AutoHeap (PartialResolution World) -> String
+dump_each_decision aheap = case first of
+                Just pr -> "((" ++ show (AutoHeap.size aheap) ++ ")) " ++ dump_pr pr ++ "\n" ++ dump_each_decision others
+                Nothing -> "((" ++ show (AutoHeap.size aheap) ++ ")) " ++ "no more"
     where first = AutoHeap.query aheap
           others = AutoHeap.remove_head aheap
 
-dump3 :: (PartialResolution World) -> String
-dump3 (PartialResolution priority (PartialSolution intent world cost stack))
+dump_pr :: (PartialResolution World) -> String
+dump_pr (PartialResolution priority (PartialSolution intent world cost stack))
   = "PartialResolution: "
   ++ " priority=" ++ show priority
   ++ " goal=" ++ goal_name (intent_goal intent)
-  ++ show_task (intent_task intent)
+  ++ dump_task (intent_task intent)
   ++ " cost=" ++ show cost
-  ++ " stacksize=" ++ show (length stack)
-  ++ if null stack then "" else " head: goal=" ++ goal_name (intent_goal (head stack))
-  ++ if null stack then "" else show_task (intent_task (head stack))
+  ++ " stack: " ++ dump_stack stack
+  -- ++ " stacksize=" ++ show (length stack)
+  -- ++ if null stack then "" else " head: goal=" ++ goal_name (intent_goal (head stack))
+  -- ++ if null stack then "" else show_task (intent_task (head stack))
   ++ dump_world world
-      where show_task Nothing = ""
-            show_task (Just task) = " task=" ++ task_name task
+            
+dump_stack :: [Intent world] -> String
+dump_stack [] = ""
+dump_stack (intent : intents) = dump_stack intents ++ " -> " ++ goal_name (intent_goal intent) ++ perhaps_dump (\intent -> " => " ++ task_name intent) (intent_task intent) ++ " "
+
+dump_task :: Maybe (Task world) -> String
+dump_task Nothing = ""
+dump_task (Just task) = " task=" ++ task_name task
+
+perhaps_do :: b -> (a -> b) -> Maybe a -> b
+perhaps_do default_string _ Nothing = default_string
+perhaps_do _ func (Just a) = func a
+
+perhaps_dump :: (a -> String) -> Maybe a -> String
+perhaps_dump = perhaps_do ""
 
 main :: IO ()
 main = do
-    doit 99 (start_resolving bountiful_world [Desire (be_unhungry 1) (const 44)])
+    doit 2 (start_resolving bountiful_world [Desire (be_unhungry 1) (const 44)])
     return ()

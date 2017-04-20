9479924e91ef019e27e2bf4d862ed0d0027c313a
Corey Knight <corey.knight@learnosity.com>
2017-04-21 08:53:32 +1000

    investigating explosions. good data so far

--- autonomy/TestUninformedScan.hs
+++ autonomy/TestUninformedScan.hs
@@ -33,7 +33,7 @@ assert_equal x y = if x == y
 
 hungry_world = Map.fromList [(1, Actor 1 14 (Map.fromList [("dude", 1), ("hunger", 52)]))]
 bountiful_world = Map.fromList [
-    (1, Actor 1 14 (Map.fromList [("dude", 1), ("hunger", 52), ("prepped_ingredients", 52)])),
+    (1, Actor 1 14 (Map.fromList [("dude", 1), ("hunger", 32), ("prepped_ingredients", 52)])),
     (2, Actor 2 18 (Map.fromList [("oven", 1)])),
     (3, Actor 3 0 (Map.fromList [("oven", 1)])),
     (4, Actor 4 44 (Map.fromList [("food", 2)])),
@@ -53,7 +53,7 @@ cookable_world = Map.fromList [
 -- 3) [take_item_food, take_item_food, cook, cook] (skipped)
 -- 4) [goto_food, goto_food, goto_oven, goto_oven]
     
-run_simulation :: Verbosity -> Int -> (Int, Int, Int) -> AutoHeap (PartialResolution World) -> IO ()
+run_simulation :: Verbosity -> Int -> (Int, AutoHeap (PartialResolution World), Int) -> AutoHeap (PartialResolution World) -> IO ()
 run_simulation _ 0 _ _ = return ()
 run_simulation verbosity n statistics aheap = case AutoHeap.query aheap of
                  Nothing -> putStrLn "Aborted -- empty queue."
@@ -63,24 +63,29 @@ run_simulation verbosity n statistics aheap = case AutoHeap.query aheap of
                                    where is_victorious (PartialResolution _ (PartialSolution intent world _ _)) = goal_succeeds (intent_goal intent) world
                                          print_status = case verbosity of
                                                             NoVerbosity -> return ()
-                                                            StepCounter -> putStrLn ("Step: " ++ show n) >> putStrLn (dump_decisions aheap)
+                                                            StepCounter -> putStrLn ("Step: " ++ show n) >> putStrLn (dump_decisions True aheap)
                                                             FullInfo -> putStrLn (show (AutoHeap.size aheap))
-                                         print_outcomes (num_steps, biggest_breadth, total_breadths) = putStrLn ("Steps: " ++ show num_steps ++ ", biggest breadth: " ++ show biggest_breadth ++ ", average breadth: " ++ show (fromIntegral total_breadths / fromIntegral num_steps))
-                                         increment_statistics (num_steps, biggest_breadth, total_breadths) = (num_steps + 1, max biggest_breadth (AutoHeap.size aheap), total_breadths + (AutoHeap.size aheap))
+                                         print_outcomes (num_steps, broadest, total_breadths)
+                                            = putStrLn ("Steps: " ++ show num_steps ++ ", biggest breadth: " ++ show (AutoHeap.size broadest) ++ ", average breadth: " ++ show (fromIntegral total_breadths / fromIntegral num_steps))
+                                                >> putStrLn (dump_decisions False broadest)
+                                         increment_statistics (num_steps, broadest, total_breadths) =
+                                            (num_steps + 1,
+                                            if AutoHeap.size aheap > AutoHeap.size broadest then aheap else broadest,
+                                            total_breadths + (AutoHeap.size aheap))
                                          dump_this_intent (PartialResolution _ (PartialSolution intent _ _ _)) = dump_stack [intent]
 
-dump_decisions :: AutoHeap (PartialResolution World) -> String
-dump_decisions aheap = dump_each_decision aheap ++ "\n"
+dump_decisions :: Bool -> AutoHeap (PartialResolution World) -> String
+dump_decisions should_dump_world aheap = dump_each_decision should_dump_world aheap ++ "\n"
 
-dump_each_decision :: AutoHeap (PartialResolution World) -> String
-dump_each_decision aheap = case first of
-                Just pr -> "((" ++ show (AutoHeap.size aheap) ++ ")) " ++ dump_pr pr ++ "\n" ++ dump_each_decision others
+dump_each_decision :: Bool -> AutoHeap (PartialResolution World) -> String
+dump_each_decision should_dump_world aheap = case first of
+                Just pr -> "((" ++ show (AutoHeap.size aheap) ++ ")) " ++ dump_pr should_dump_world pr ++ "\n" ++ dump_each_decision should_dump_world others
                 Nothing -> "((" ++ show (AutoHeap.size aheap) ++ ")) " ++ "no more"
     where first = AutoHeap.query aheap
           others = AutoHeap.remove_head aheap
 
-dump_pr :: (PartialResolution World) -> String
-dump_pr (PartialResolution priority (PartialSolution intent world cost stack))
+dump_pr :: Bool -> (PartialResolution World) -> String
+dump_pr should_dump_world (PartialResolution priority (PartialSolution intent world cost stack))
   = "PartialResolution: "
   ++ " priority=" ++ show priority
   ++ " goal=" ++ goal_name (intent_goal intent)
@@ -90,7 +95,7 @@ dump_pr (PartialResolution priority (PartialSolution intent world cost stack))
   -- ++ " stacksize=" ++ show (length stack)
   -- ++ if null stack then "" else " head: goal=" ++ goal_name (intent_goal (head stack))
   -- ++ if null stack then "" else show_task (intent_task (head stack))
-  ++ dump_world world
+  ++ if should_dump_world then dump_world world else ""
             
 dump_stack :: [Intent world] -> String
 dump_stack [] = ""
@@ -109,5 +114,5 @@ perhaps_dump = perhaps_do ""
 
 main :: IO ()
 main = do
-    run_simulation NoVerbosity 9999 (0, 0, 0) (start_resolving bountiful_world [Desire (be_unhungry 1) (const 44)])
+    run_simulation NoVerbosity 9999 (0, AutoHeap.void (const (const EQ)), 0) (start_resolving bountiful_world [Desire (be_unhungry 1) (const 44)])
     return ()

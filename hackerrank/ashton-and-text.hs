55e12a30aef2e1c42c96fc23c250b18b3463d7c7
Corey Knight <corey.knight@learnosity.com>
2017-08-02 08:43:20 +1000

    ashton-and-text now partially works (but still fails on big cases)

--- hackerrank/ashton-and-text.hs
+++ hackerrank/ashton-and-text.hs
@@ -1,14 +1,12 @@
 
-import qualified Data.Text as Text
+import Control.Lens.Operators
+import Data.List
+import Data.Set(Set(..))
 import Data.Text(Text(..))
+import qualified Data.Set as Set
+import qualified Data.Text as Text
 import qualified Data.Text.IO as TextIO
 import qualified Data.Text.Read as TextRead
-import Data.List
-import Control.Monad
-import Control.Lens.Operators
-import Control.Monad.Reader
-import qualified Data.Set as Set
-import Data.Set(Set(..))
 
 main = do
     numCasesLine <- TextIO.getLine
@@ -25,22 +23,13 @@ doTestCase = do
     let char = getFromSubsequences index text in
         putChar char >> putChar '\n'
 
---textSubsequences :: Text -> [Text]
---textSubsequences
-
 getFromSubsequences :: Int -> Text -> Char
-getFromSubsequences index text = (Text.concat . map head . group . sort . textSubsequences) text `Text.index` index
+getFromSubsequences index text = (Text.concat . Set.toList . textSubsequences) text `Text.index` index
 
-textSubsequences :: Text -> [Text]
-textSubsequences x = [x]
-
--- filters duplicates from a sorted list
-uniq :: Eq a => [a] -> [a]
-uniq [] = []
-uniq (x:[]) = [x]
-uniq (x:y:zs)
-  | x == y = uniq (x:zs)
-  | otherwise = x : uniq (y:zs)
+textSubsequences :: Text -> Set Text
+textSubsequences text
+  | Text.null text = Set.empty
+  | otherwise = Set.fromList (Text.tails text) `Set.union` textSubsequences (Text.dropEnd 1 text)
 
 getIndex :: IO Int
 getIndex = TextIO.getLine <&> \text -> either error (subtract 1 . fst) (TextRead.decimal text)

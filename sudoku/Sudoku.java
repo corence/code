9a67a049a5674a3ab2660611d4bc11212e614740
Corey Knight <corey.knight@learnosity.com>
2017-08-29 10:39:08 +1000

    java is looking pretty good but it's bugged (it put a 0 in a cell)

--- sudoku/Sudoku.java
+++ sudoku/Sudoku.java
@@ -2,6 +2,18 @@
 import java.util.*;
 
 public class Sudoku {
+    public static <T> String dump(Iterable<T> iterable) {
+        StringBuilder sb = new StringBuilder("[");
+        for(T t : iterable) {
+            sb.append(t.toString()).append(",");
+        }
+        if(sb.length() > 1) {
+            sb.setLength(sb.length() - 1);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
     public static class Pos {
         public int x;
         public int y;
@@ -19,6 +31,10 @@ public class Sudoku {
             Pos that = (Pos)rhs;
             return this.x == that.x && this.y == that.y;
         }
+
+        public String toString() {
+            return "{" + x + "," + y + "}";
+        }
     }
 
     public static class Cell {
@@ -34,8 +50,29 @@ public class Sudoku {
             this.groups = new ArrayList<Group>();
         }
 
-        public void setValue(int newValue) {
-            throw new RuntimeException();
+        public void setValue(Integer newValue) {
+            if(!possibilities.contains(newValue)) {
+                throw new IllegalStateException("value " + newValue + " doesn't exist in " + possibilities.size() + " possibilities");
+            }
+
+            if(value > 0) {
+                throw new IllegalStateException("can't set value to " + newValue + " because it is already " + value);
+            }
+
+            value = newValue;
+            //System.out.println("clearing possibilities: " + dump(possibilities));
+            possibilities.clear();
+
+            //System.out.println("groups: " + groups.size());
+            for(Group group : groups) {
+                //System.out.println("cells: " + group.cells.size());
+                for(Cell cell : group.cells) {
+                    //System.out.println("removing " + newValue + " from a cell at " + cell.pos + " with possibilities: " + dump(cell.possibilities));
+                    cell.possibilities.remove(newValue);
+                }
+                group.cells.remove(this);
+            }
+            groups.clear();
         }
     }
 
@@ -44,6 +81,9 @@ public class Sudoku {
 
         public Group(List<Cell> cells) {
             this.cells = new ArrayList<Cell>(cells);
+            for(Cell cell : cells) {
+                cell.groups.add(this);
+            }
         }
     }
 
@@ -105,6 +145,14 @@ public class Sudoku {
         }
     }
 
+    public static <T> String displayElement(List<T> elements, int index) {
+        if(index >= elements.size()) {
+            return " ";
+        } else {
+            return elements.get(index).toString();
+        }
+    }
+
     public String display() {
         StringBuilder sb = new StringBuilder();
 
@@ -116,29 +164,33 @@ public class Sudoku {
 
         for(int y = 0; y < 9; ++y) {
             for(int x = 0; x < 9; ++x) {
+                sb.append('|');
                 Cell cell = this.cells.get(new Pos(x, y));
-                System.out.println(x + ", " + y);
-                if(!cell.possibilities.isEmpty()) {
-                    sb.append("|" + cell.possibilities.get(0) + cell.possibilities.get(1) + cell.possibilities.get(2));
-                }
+                sb.append(displayElement(cell.possibilities, 0));
+                sb.append(displayElement(cell.possibilities, 1));
+                sb.append(displayElement(cell.possibilities, 2));
             }
             sb.append("|\n");
 
             for(int x = 0; x < 9; ++x) {
+                sb.append('|');
                 Cell cell = this.cells.get(new Pos(x, y));
                 if(cell.possibilities.isEmpty()) {
-                    sb.append("| " + cell.value + " ");
+                    sb.append(" " + cell.value + " ");
                 } else {
-                    sb.append("|" + cell.possibilities.get(3) + cell.possibilities.get(4) + cell.possibilities.get(5));
+                    sb.append(displayElement(cell.possibilities, 3));
+                    sb.append(displayElement(cell.possibilities, 4));
+                    sb.append(displayElement(cell.possibilities, 5));
                 }
             }
             sb.append("|\n");
 
             for(int x = 0; x < 9; ++x) {
+                sb.append('|');
                 Cell cell = this.cells.get(new Pos(x, y));
-                if(!cell.possibilities.isEmpty()) {
-                    sb.append("|" + cell.possibilities.get(6) + cell.possibilities.get(7) + cell.possibilities.get(8));
-                }
+                sb.append(displayElement(cell.possibilities, 6));
+                sb.append(displayElement(cell.possibilities, 7));
+                sb.append(displayElement(cell.possibilities, 8));
             }
             sb.append("|\n ");
 
@@ -151,8 +203,47 @@ public class Sudoku {
         return sb.toString();
     }
 
+    public static void addSampleBoard(Sudoku sudoku) {
+        sudoku.cells.get(new Pos(0, 0)).setValue(5);
+        sudoku.cells.get(new Pos(0, 1)).setValue(8);
+        sudoku.cells.get(new Pos(1, 1)).setValue(3);
+        sudoku.cells.get(new Pos(1, 2)).setValue(1);
+        sudoku.cells.get(new Pos(1, 3)).setValue(4);
+        sudoku.cells.get(new Pos(1, 7)).setValue(7);
+        sudoku.cells.get(new Pos(2, 1)).setValue(2);
+        sudoku.cells.get(new Pos(2, 4)).setValue(6);
+        sudoku.cells.get(new Pos(2, 5)).setValue(7);
+        sudoku.cells.get(new Pos(3, 1)).setValue(1);
+        sudoku.cells.get(new Pos(3, 3)).setValue(6);
+        sudoku.cells.get(new Pos(3, 4)).setValue(2);
+        sudoku.cells.get(new Pos(3, 6)).setValue(7);
+        sudoku.cells.get(new Pos(3, 7)).setValue(8);
+        sudoku.cells.get(new Pos(4, 1)).setValue(5);
+        sudoku.cells.get(new Pos(4, 7)).setValue(2);
+        sudoku.cells.get(new Pos(5, 1)).setValue(4);
+        sudoku.cells.get(new Pos(5, 2)).setValue(2);
+        sudoku.cells.get(new Pos(5, 4)).setValue(1);
+        sudoku.cells.get(new Pos(5, 5)).setValue(8);
+        sudoku.cells.get(new Pos(6, 3)).setValue(1);
+        sudoku.cells.get(new Pos(6, 4)).setValue(4);
+        sudoku.cells.get(new Pos(6, 7)).setValue(5);
+        sudoku.cells.get(new Pos(7, 1)).setValue(6);
+        sudoku.cells.get(new Pos(7, 5)).setValue(5);
+        sudoku.cells.get(new Pos(7, 6)).setValue(2);
+        sudoku.cells.get(new Pos(7, 7)).setValue(9);
+        sudoku.cells.get(new Pos(8, 7)).setValue(1);
+        sudoku.cells.get(new Pos(8, 8)).setValue(3);
+    }
+
     public static void main(String[] args) {
-        System.out.println(new Sudoku().display());
+        Sudoku sudoku = new Sudoku();
+        addSampleBoard(sudoku);
+        System.out.println(sudoku.display());
+        for(int i=0; i<30; ++i) {
+            System.out.println("* " + i + " *");
+            sudoku.reduce();
+            System.out.println(sudoku.display());
+        }
     }
 
     public void reduce() {

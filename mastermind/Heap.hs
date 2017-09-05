d28a57e3650ee798379b7e9a4dbb16dd19373aea
Corey Knight <corey.knight@learnosity.com>
2017-09-05 14:13:43 +1000

    mastermind is done, except it doesn't work

--- mastermind/Heap.hs
+++ mastermind/Heap.hs
@@ -1,16 +1,10 @@
 
-module Heap
-( Heap(..)
-, add
-, query
-, remove_head
-, size
-, void
-) where
+module Heap where
 
 type Entry key value = (key, value)
 data Heap key value = Heap Int (Maybe (key, value)) (Heap key value) (Heap key value) -- size, key, value, left_child, right_child
-void = Heap 0 Nothing void void
+    deriving (Show)
+empty = Heap 0 Nothing empty empty
 
 add :: Ord key => (key, value) -> Heap key value -> Heap key value
 add entry (Heap p_size Nothing p_left p_right) = Heap (p_size + 1) (Just entry) p_left p_right
@@ -33,7 +27,7 @@ remove_head (Heap p_size p_entry p_left p_right) = delete_empty_entry (Heap (p_s
 
 delete_empty_entry :: Ord key => Heap key value -> Heap key value
 delete_empty_entry (Heap p_size Nothing p_left p_right)
-  | size p_left == 0 && size p_right == 0 = void -- terminate if both children are empty
+  | size p_left == 0 && size p_right == 0 = empty -- terminate if both children are empty
   | higher_priority (entry p_left) (entry p_right) = Heap p_size (entry p_left) (remove_head p_left) p_right -- 
   | otherwise = Heap p_size (entry p_right) p_left (remove_head p_right)
   where entry (Heap _ entry _ _) = entry
@@ -47,3 +41,6 @@ values = map snd . toList
 
 toList :: Ord key => Heap key value -> [(key, value)]
 toList heap@(Heap _ entry _ _) = maybe [] (: toList (remove_head heap)) entry
+
+fromList :: Ord key => [(key, value)] -> Heap key value
+fromList = foldr Heap.add Heap.empty
--- mastermind/Mastermind.hs
+++ mastermind/Mastermind.hs
@@ -4,29 +4,32 @@ import Data.Set(Set(..))
 import Data.List
 import qualified Heap as Heap
 import Heap(Heap(..))
+import Data.Char
 
 (&) = flip ($)
 
 type Index = Int
-type Value = Int
+type Value = Char
 type Depth = Int
 type Pattern = [Value]
 
-data Estimate = Estimate Depth Pattern
-data Attempt = Attempt Pattern Int Int -- matched positions, unmatched positions
+data Estimate = Estimate Depth Pattern deriving Show
+data Attempt = Attempt Pattern Int Int deriving Show -- matched positions, unmatched positions
 
-depth [] = 1
-depth (0:pattern) = patternLength * depth pattern
-depth (_:pattern) = depth pattern
+numZeroes :: Pattern -> Int
+numZeroes = length . filter (== '.')
+
+numMatchy :: Attempt -> Int
+numMatchy (Attempt _ matchy _) = matchy
 
 estimate :: Pattern -> Estimate
-estimate pattern = Estimate (depth pattern) pattern
+estimate pattern = Estimate (numZeroes pattern) pattern
 
-patternLength = 4
-initialPattern = take patternLength $ repeat 0
+initialPattern :: Int -> Pattern
+initialPattern patternLength = take patternLength $ repeat '.'
 
 makePatterns :: Int -> Pattern -> Index -> [Pattern]
-makePatterns patternLength pattern index = map insertValue [0..(patternLength - 1)]
+makePatterns numValues pattern index = map insertValue ['a'..(chr ((ord 'a') + numValues - 1))]
     where insertValue value = take (index - 1) pattern ++ [value] ++ drop index pattern -- probably there's a library function for this
 
 -- ....
@@ -38,25 +41,32 @@ makePatterns patternLength pattern index = map insertValue [0..(patternLength - 
 -- to expand it:
 --  - remove it from the list
 --  - put all expansions back into the list
--- keep doing this until we expand a full-length response (depth = 1)
+-- keep doing this until we expand a full-length response (numZeroes = 0)
 
 -- given a bunch of possibilities
--- randomly pick one (if that's too hard then pick one with the highest depth)
+-- randomly pick one (if that's too hard then pick one with the highest numZeroes)
 -- if it is fully-expanded: attempt it
 -- if not: remove it, expand it, put its children in
-type Status = (Pattern, Collection, [Attempt])
+type Status = (Int, Pattern, Collection, [Attempt])
 
 type Collection = Heap Int Estimate
 
+reduce :: Status -> Status
+reduce status@(numValues, answer, collection, attempts) =
+    if (filter isSolution attempts & length & (> 0))
+        then status
+        else reduce $ action status
+        where isSolution attempt = numMatchy attempt == length answer
+
 action :: Status -> Status
-action status@(answer, collection, attempts) =
+action status@(numValues, answer, collection, attempts) =
     case Heap.query collection of
         Nothing -> status
-        Just (Estimate 1 pattern) -> makeAttempt pattern status
-        Just (Estimate _ pattern) -> (answer, expandInCollection pattern collection, attempts)
-        where expandInCollection pattern collection = foldr add (Heap.remove_head collection) (expand attempts pattern)
-              add pattern = Heap.add (negate $ depth pattern, Estimate (depth pattern) pattern)
+        Just (Estimate 0 pattern) -> makeAttempt pattern status
+        Just (Estimate _ pattern) -> (numValues, answer, expandInCollection pattern collection, attempts)
+        where expandInCollection pattern collection = foldr addToCollection (Heap.remove_head collection) (expand numValues attempts pattern)
 
+addToCollection pattern = Heap.add (numZeroes pattern, Estimate (numZeroes pattern) pattern)
 {-
 action :: Status -> Status
 action status@(collection, attempts)
@@ -70,14 +80,14 @@ action status@(collection, attempts)
 -}
 
 makeAttempt :: Pattern -> Status -> Status
-makeAttempt pattern status@(answer, collection, attempts) = (answer, collection, attempt : attempts)
+makeAttempt pattern status@(numValues, answer, collection, attempts) = (numValues, answer, collection, attempt : attempts)
     where attempt = Attempt pattern matchy unmatchy
           (matchy, unmatchy) = score answer pattern
 
-expand :: [Attempt] -> Pattern -> [Pattern]
-expand attempts pattern
-    = [0..(patternLength - 1)] -- we now have a [Index]
-    & map (makePatterns patternLength pattern) -- we now have a [[Pattern]]
+expand :: Int -> [Attempt] -> Pattern -> [Pattern]
+expand numValues attempts pattern
+    = [0..(numValues - 1)] -- we now have a [Index]
+    & map (makePatterns numValues pattern) -- we now have a [[Pattern]]
     & concat -- we now have a [Pattern]
     & filter (\pattern -> all (satisfiesAttempt pattern) attempts) -- foreach pattern: it should satisfy all attempts
 
@@ -104,7 +114,9 @@ unmatchyScore (x:xs) ys
   | elem x ys = 1 + unmatchyScore xs (delete x ys)
   | otherwise = unmatchyScore xs (tail ys)
 
-main = pure ()
+main = do
+    putStrLn $ show $ initialPattern 3
+    putStrLn $ show $ reduce (3, "abc", addToCollection (initialPattern 3) Heap.empty, [])
 
 -- assume pattern is RRO
 -- RGR 1 1
